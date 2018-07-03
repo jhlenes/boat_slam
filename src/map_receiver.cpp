59475d48a8f40ffd358c3aed57f0abeb63a330e3
Jan Henrik Lenes <jhlenes@outlook.com>
2018-07-03 14:10:24 +0000

    Start implementing Complete Coverage algorithm.

--- src/map_receiver.cpp
+++ src/map_receiver.cpp
@@ -1,22 +1,63 @@
 #include <ros/ros.h>
-#include <stdio.h>
-
+#include <tf2_ros/transform_listener.h>
 #include <nav_msgs/OccupancyGrid.h>
+#include <nav_msgs/Path.h>
 
-void mapCallback(const nav_msgs::OccupancyGrid &map){
+#include <queue>
+#include <string>
+#include <algorithm>
 
-    ROS_INFO("Map received!");
-    const unsigned int height = map.info.height;
-    const unsigned int width = map.info.width;
-    ROS_INFO_STREAM("Height: " << height << "\tWidth: " << width);
+double x = 0.0;
+double y = 0.0;
+
+struct cell {
+    int row;
+    int col;
+};
+
+void mapCallback(const nav_msgs::OccupancyGrid &grid){
+
+    uint32_t height = grid.info.height;
+    uint32_t width = grid.info.width;
+
+    // Find grid position of robot
+    float resolution = grid.info.resolution;
+    double originX = grid.info.origin.position.x;
+    double originY = grid.info.origin.position.x;
+    ROS_WARN_STREAM("X: " << originX);
+    int row = (x - originX) / resolution;
+    int col = (y - originY) / resolution;
+
+    // generate distance transform with BFS
+    std::queue<cell> toVisit;
+    bool visited[height][width];
+    cell start = {row, col};
+    toVisit.push(start);
+    int distanceTransform[height][width];
+    distanceTransform[row][col] = 0;
+    visited[row][col] = true;
+
+    while (!toVisit.empty()) {
+        cell c = toVisit.pop();
+
+        for (int i = std::max(c.row-1, 0); i < std::min(c.row+1, height-1); i++) {
+            for (int j = std::max(c.col-1, 0); j < std::min(c.col+1, width-1); j++) {
+                if (!visited[i][j]) {
+                    cell newCell = {i, j};
+                    toVisit.push(newCell);
+                    distanceTransform[i][j] == distanceTransform[c.row][c.col]+1;
+                    visited[i][j] = true;
+                }
+            }
+        }
+    }
 
     for (size_t row = 0; row < height; ++row) {
         for (size_t col = 0; col < width; ++col) {
-            const signed char a = map.data[row*width + col];
-            std::cout << int(a) << " ";
+            int8_t prob = grid.data[row*width + col];
         }
-        std::cout << "\n";
     }
+
 }
 
 int main(int argc, char** argv){
@@ -25,6 +66,28 @@ int main(int argc, char** argv){
 
     ros::Subscriber sub = node.subscribe("map", 1000, mapCallback);
 
-    ros::spin();
+    tf2_ros::Buffer tfBuffer;
+    tf2_ros::TransformListener tfListener(tfBuffer);
+
+    ros::Rate rate(10.0);
+    while (node.ok()) {
+
+        // get the position
+        geometry_msgs::TransformStamped transformStamped;
+        try{
+          transformStamped = tfBuffer.lookupTransform("base_link", "map", ros::Time(0.0), ros::Duration(0.0));
+        }
+        catch (tf2::TransformException &ex) {
+          ROS_WARN("Transform from map to base_link not found.");
+          continue;
+        }
+
+        x = transformStamped.transform.translation.x;
+        y = transformStamped.transform.translation.y;
+
+        ros::spinOnce();
+        rate.sleep();
+    }
+
     return 0;
 }
