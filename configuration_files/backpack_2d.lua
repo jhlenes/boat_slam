c7efc2d08ceef4e1f8497038b13daefab809a843
Jan Henrik Lenes <jhlenes@outlook.com>
2018-07-11 10:59:59 +0000

    Fix configs for simulations

--- configuration_files/backpack_2d.lua
+++ configuration_files/backpack_2d.lua
@@ -29,6 +29,6 @@ options = {
 }
 
 MAP_BUILDER.use_trajectory_builder_2d = true
-TRAJECTORY_BUILDER_2D.num_accumulated_range_data = 10
+TRAJECTORY_BUILDER_2D.num_accumulated_range_data = 1
 
 return options
